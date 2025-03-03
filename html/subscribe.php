1c695b3187acd57f15dabbb45fc7dc4e878aac75
Eduardo Sanchez <walkovik@gmail.com>
2025-03-03 03:52:52 -0500

    Fixes

--- html/subscribe.php
+++ html/subscribe.php
@@ -2,7 +2,7 @@
 <section id="subscribe" class="">
 	<div class="row">
 		<div class="col-xs-12">
-			<img src="static/img/subscribe.jpg" alt="subscribe" class="subscribe" />
+			<img src="static/img/subscribe.jpg" alt="subscribe" class="responsive" />
 		</div>
 	</div>
 </section>
