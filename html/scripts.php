623ba060275ef29558e92427236b9983ea541067
Eduardo Sanchez <walkovik@gmail.com>
2025-05-14 21:47:47 -0400

    Refactorizado index, codigo movido a nuevos archivos

---
+++ html/scripts.php
@@ -0,0 +1,41 @@
+<!-- Scripts -->
+<script>
+	const CONFIG = {
+		siteName: <?= json_encode(defined('SITE_NAME') ? SITE_NAME : '') ?>,
+		supportEmail: <?= json_encode(defined('SUPPORT_EMAIL') ? SUPPORT_EMAIL : '') ?>,
+		apiUrl: <?= json_encode(defined('API_URL') ? API_URL : '') ?>,
+		local_env: <?= json_encode(defined('LOCAL_ENV') ? LOCAL_ENV : false) ?>
+	};
+</script>
+<script type="text/javascript"
+        src="static/js/vendor/bootstrap.min.js"></script>
+<script type="text/javascript"
+        src="static/js/vendor/classie.js"></script>
+<script type="text/javascript"
+        src="static/js/vendor/respond.js"></script>
+<script type="text/javascript"
+        src="static/js/vendor/jquery.counterup.js"></script>
+<script type="text/javascript"
+        src="static/js/vendor/waypoints.min.js"></script>
+<script type="text/javascript"
+        src="static/js/vendor/smoothscroll.js"></script>
+<script type="text/javascript"
+        src="static/js/vendor/jquery.backstretch.min.js"></script>
+<script type="text/javascript"
+        src="static/js/vendor/jquery.backgroundvideo.min.js"></script>
+<script type="text/javascript"
+        src="static/js/vendor/wow.js"></script>
+<script type="text/javascript"
+        src="static/js/vendor/detectmobilebrowser.js"></script>
+<script type="text/javascript"
+        src="static/js/vendor/owl.carousel.min.js"></script>
+<script type="text/javascript"
+        src="static/js/vendor/imagesloaded.pkgd.min.js"></script>
+<script type="text/javascript"
+        src="static/js/vendor/masonry.pkgd.min.js"></script>
+<script type="text/javascript"
+        src="static/js/vendor/cbpGridGallery.js"></script>
+<script type="text/javascript"
+        src="static/js/vendor/styleswitch.js"></script>
+<script type="text/javascript"
+        src="static/js/script.js"></script>
