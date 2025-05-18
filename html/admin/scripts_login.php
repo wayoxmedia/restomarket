e13e32b954a8f5480e60794e622ca5552aa5422d
Eduardo Sanchez <walkovik@gmail.com>
2025-05-18 01:40:47 -0400

    Refactored

--- html/admin/scripts_login.php
+++ html/admin/scripts_login.php
@@ -3,7 +3,7 @@
     siteName: <?= json_encode(defined('SITE_NAME') ? SITE_NAME : '') ?>,
     supportEmail: <?= json_encode(defined('SUPPORT_EMAIL') ? SUPPORT_EMAIL : '') ?>,
     apiUrl: <?= json_encode(defined('API_URL') ? API_URL : '') ?>,
-    local_env: <?= json_encode(defined('LOCAL_ENV') ? LOCAL_ENV : false) ?>
+    local_env: <?= json_encode(defined('LOCAL_ENV') && LOCAL_ENV) ?>
   };
 </script>
 
