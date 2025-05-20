fda74679cc5aa53852105ba1df7283d052750a1b
Eduardo Sanchez <walkovik@gmail.com>
2025-05-20 02:28:43 -0400

    Using Bootstrap JS CDN

--- html/admin/head.php
+++ html/admin/head.php
@@ -17,7 +17,11 @@
       local_env: <?= json_encode(defined('LOCAL_ENV') && LOCAL_ENV) ?>
     };
   </script>
-  <script src="../static/admin/libs/jquery/dist/jquery.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+          integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+          crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+          crossorigin="anonymous"></script>
   <script src="../static/admin/js/isLoggedIn.js"></script>
-  <script src="../static/admin/js/bootstrap.bundle.js"></script>
 </head>
