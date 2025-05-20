dffded5faaa81706ee961d7504da0615ddab08a6
Eduardo Sanchez <walkovik@gmail.com>
2025-05-20 02:00:04 -0400

    Using Libs from CDN

--- html/admin/foot.php
+++ html/admin/foot.php
@@ -1,7 +1,7 @@
 <script src="../static/admin/js/sidebarMenu.js"></script>
 <script src="../static/admin/js/app.js"></script>
-<script src="../static/admin/libs/apexcharts/dist/apexcharts.min.js"></script>
-<script src="../static/admin/libs/simplebar/dist/simplebar.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+<script src="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.min.js"></script>
 <script src="../static/admin/js/dashboard.js"></script>
 <!-- solar icons -->
 <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
