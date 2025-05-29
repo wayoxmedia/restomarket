b99449b88b78e6735c0b37165caec1180c08ebbe
Luis Jean Linares <luisjeancarlo@yahoo.es>
2025-05-28 21:12:25 -0400

    Texto arepa agregado

--- html/arepas.php
+++ html/arepas.php
@@ -4,7 +4,7 @@
 		<div class="col-xs-12">
 			<img src="static/img/arepas_combos.jpg" alt="arepas combos" class="responsive" />
 			<div class="btn-arepas-container">
-				<h1 class="btn-x-small-red">COMBO<br><span class="white">AREPAS</span></h1>
+				<h1 class="btn-x-small-red">COMBO<br><span class="white"><?= TEXTO_AREPAS?></span></h1>
 				<p class="red txt-x-small" style="line-height: 1.2;">
 					Estas arepas se las traen, con los mejores rellenos, tu paladar nunca será el mismo luego de probarlas.
 				</p>
