edc910bd75136a82a77ad01ed642ead5b4d20070
Eduardo Sanchez <walkovik@gmail.com>
2025-06-01 03:27:07 -0400

    Added Subscribers Table

--- html/admin/head.php
+++ html/admin/head.php
@@ -5,7 +5,10 @@
   <!-- CSS -->
   <link rel="shortcut icon" type="image/png" href="../favicon.svg" />
   <link rel="stylesheet" href="../static/admin/css/styles.min.css" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.css"/>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.css" />
+  <link rel="stylesheet" href="https://cdn.datatables.net/2.3.1/css/dataTables.dataTables.min.css" />
+  <link rel="stylesheet" href="https://cdn.datatables.net/2.3.1/css/dataTables.bootstrap5.css" />
+  <link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.2.3/css/buttons.dataTables.css" />
   <link rel="stylesheet" href="../static/admin/css/custom.css" />
 
   <!-- JS -->
@@ -15,6 +18,16 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
           crossorigin="anonymous"></script>
+  <script src="https://cdn.datatables.net/2.3.1/js/dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/3.2.3/js/dataTables.buttons.js"></script>
+  <script src="https://cdn.datatables.net/buttons/3.2.3/js/buttons.dataTables.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+  <script src="https://cdn.datatables.net/buttons/3.2.3/js/buttons.html5.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/3.2.3/js/buttons.print.min.js"></script>
+  <script src="https://cdn.datatables.net/2.3.1/js/dataTables.bootstrap5.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/dayjs@1/dayjs.min.js"></script>
   <script type="module" src="../static/admin/js/global.js"></script>
   <script type="module" src="../static/admin/js/isLoggedIn.js"></script>
 </head>
--- html/static/admin/js/global.js
+++ html/static/admin/js/global.js
@@ -1,3 +1,5 @@
+import { msaConfig } from './config.js.php';
+
 let spinnerCount = 0;
 
 /**
@@ -17,4 +19,39 @@ export function hideSpinner() {
   if (spinnerCount > 0) spinnerCount--;
   // If there are still spinners, do not hide the overlay.
   if (spinnerCount === 0 && overlay) overlay.style.display = 'none';
+}
+
+/**
+ * Generate a report filename based on the provided parameters.
+ *
+ * @param type
+ * @param startDate
+ * @param endDate
+ * @param suffix
+ * @returns {string}
+ */
+export function reportFilename(type = "report", startDate = null, endDate = null, suffix = null) {
+  let scn = msaConfig.siteCodeName || '';
+
+  let startDateFmt = startDate && dayjs(startDate).isValid()
+    ? dayjs(startDate).format("YYYY-MM-DD")
+    : startDate;
+  let endDateFmt = endDate
+    ? (dayjs(endDate).isValid()
+      ? `_to_${dayjs(endDate).format("YYYY-MM-DD")}`
+      : `_to_${endDate}`)
+    : "";
+
+  suffix = suffix ? `_${suffix}`: "";
+
+  return `${scn}_${type}_${startDateFmt}${endDateFmt}${suffix}`.replace(/ /g,"_");
+}
+
+/**
+ * Check if the given item is null, undefined, or an empty string.
+ * @param item
+ * @returns {boolean}
+ */
+export function isNUE(item) {
+  return item === null || item === undefined || item === '';
 }
