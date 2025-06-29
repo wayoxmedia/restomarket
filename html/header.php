6183af42444d625764c825f6b6a513f2d4342111
Eduardo Sanchez <walkovik@gmail.com>
2025-06-28 20:10:44 -0400

    Fix

--- html/header.php
+++ html/header.php
@@ -1,30 +1,30 @@
 <!-- logo -->
 <div class="logo-wrapper">
-	<h1 class="logo">
-		<span style="text-align: center;">
-			<img src="static/img/logo.png" class="logo-resize" alt="Eglee's Gourmet"/>
-		</span>
-	</h1>
+  <h1 class="logo">
+    <span style="text-align: center;">
+      <img src="static/img/logo.png" class="logo-resize" alt="Eglee's Gourmet"/>
+    </span>
+  </h1>
 </div>
 <!-- /logo -->
 
 <!-- navigation icon -->
 <div class="navmenu-open">
-	<a href="javascript:void(0);" id="trigger-navbar"><span class="icon_menu"></span></a>        
+  <a href="javascript:void(0);" id="trigger-navbar"><span class="icon_menu"></span></a>
 </div>
 <!-- / navigation icon -->
 
 <!-- home -->
 <section id="home" class="">
-	<div class="row">
-		<div class="col-xs-12">
-			<img src="static/img/top.jpg" alt="arepas" class="responsive" />
-			<div class="btn-home-container">
-				<input type="button"
-					   value="ORDENA AHORA"
-					   class="btn-cta btn-cta-white" onclick="window.location.href='#contacto'">
-			</div>
-		</div>
-	</div>
+  <div class="row">
+    <div class="col-xs-12">
+      <img src="static/img/top.jpg" alt="arepas" class="responsive" />
+      <div class="btn-home-container">
+        <input type="button"
+             value="ORDENA AHORA"
+             class="btn-cta btn-cta-white" onclick="window.location.href='#contacto'">
+      </div>
+    </div>
+  </div>
 </section>
 <!-- /home -->
